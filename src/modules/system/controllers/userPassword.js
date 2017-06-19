38b988bb145640f045eb60cd3d01b40b10263b33
fyl080801 <fyl080801@hotmail.com>
2017-06-19 09:28:15 +0800

    password

--- src/modules/system/controllers/userPassword.js
+++ src/modules/system/controllers/userPassword.js
@@ -2,7 +2,8 @@
  * Created by fyl08 on 2017/1/4.
  */
 define('modules.system.controllers.userPassword', [
-    'modules.system.module'
+    'modules.system.module',
+    'modules.controls.requires'
 ], function (module) {
     'use strict';
 
